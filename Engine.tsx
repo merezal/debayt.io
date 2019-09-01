60c19ab16be6ee909548ec60e4ba10dc1f5f7264
MichaelJereza <35281729+MichaelJereza@users.noreply.github.com>
2019-09-01 01:29:22 -0700

    Text on button changes with input

---
+++ Engine.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Bubble from './Bubble';
+
+interface EngineState{
+    running: string;
+}
+
+let Engine: React.FC = () => {
+
+    //Content:
+    //This will be the object that holds an argument, as well as tracking id.
+    let Content = {
+        id: "x",
+        content: "Here's the argument"
+    }
+
+    return (
+       // console.log(this.props);
+            <div className="Container">
+                    <Bubble content={Content.content} />
+            </div>     
+    );
+}
+
+export default Engine;
