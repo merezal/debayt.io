b49e4a395790f7b03d1e0ee9a2484560ccccfd56
Mike <mikes@desktop>
2019-09-13 17:18:29 -0700

    Pressing escape in text field closes modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -30,7 +30,7 @@ class Modal extends React.Component<{ close: any, target_uuid: string, content_a
         this.setState({ text: x });
         /* TODO make live change to button text, rerendering modal? */
     }
-    public toggleVis = (event: React.MouseEvent<HTMLElement>) => {
+    public toggleVis = (event: React.MouseEvent<HTMLElement> | React.KeyboardEvent<HTMLElement>) => {
         this.props.close();
     }
     render() {
@@ -38,7 +38,7 @@ class Modal extends React.Component<{ close: any, target_uuid: string, content_a
                 <div id="modalBlock"  >
                     <Fab color="secondary" size="small" id="closeModal" onClick={this.toggleVis.bind(this)}>X</Fab>
                     <span className="BubbleInput">
-                        <TextField id="filled-textarea" margin="normal" multiline label="Enter Message" variant="filled" value={this.state.text} onInput={this.handleText} autoFocus />
+                        <TextField id="filled-textarea" margin="normal" multiline label="Enter Message" variant="filled" value={this.state.text} onInput={this.handleText} onKeyUp={(e) => { if (e.key == "Escape") { this.toggleVis(e) }}} autoFocus />
                     </span>
                 </div>
             );
